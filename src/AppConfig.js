7fc905c1939da14818df4d358d9111dfb68e8805
consuti-tec <consuti@consuti.com.br>
2024-09-09 21:28:23 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#064FB7",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/mb10soccer2",
+  UrlPainel: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
